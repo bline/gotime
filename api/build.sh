2a12c6002eb4d92c6559502ff63ae6c4cb45ec2e
Scott Beck <sbeck@shambhalamountain.org>
2017-11-26 17:47:36 -0700

    mv go output

--- api/build.sh
+++ api/build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-protoc -I proto/ proto/*.proto --js_out=import_style=commonjs,binary:../web/gotime/src/app/proto/ --ts_out=service=true:../web/gotime/src/app/proto/ --gogofaster_out=plugins=grpc:.
+protoc -I proto/ proto/*.proto --js_out=import_style=commonjs,binary:../web/gotime/src/app/proto/ --ts_out=service=true:../web/gotime/src/app/proto/ --gogofaster_out=plugins=grpc:./proto
 
 
