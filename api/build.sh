018691ed788ed929ee0d8551dd7c4ee7558b8208
bline <scottbeck@gmail.com>
2017-11-27 16:35:43 -0700

    switch from gogo, doesn't support ptypes

--- api/build.sh
+++ api/build.sh
@@ -4,9 +4,9 @@ bindir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 web_out="$bindir/../web/gotime/src/app/proto/"
 go_out="$bindir/proto/"
 
-protoc -I proto/ proto/*.proto \
+protoc -I $bindir/proto/ $bindir/proto/*.proto \
 	--js_out=import_style=commonjs,binary:$web_out \
 	--ts_out=service=true:$web_out \
-	--gogofaster_out=plugins=grpc:$go_out
+	--go_out=plugins=grpc:$go_out
 
 
