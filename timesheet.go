1ed4cbffdbcdd979049ffc9221d5d66cc39a37c3
Scott Beck <sbeck@shambhalamountain.org>
2017-11-27 21:15:26 -0700

    a good start

--- timesheet.go
+++ timesheet.go
@@ -1,11 +1,24 @@
 package gotime
 
 import (
+	"context"
+
+	"github.com/bline/gotime"
 	"github.com/bline/gotime/api/proto"
-
-
+	"github.com/bline/gotime/db"
+	"github.com/bline/gotime/config"
+	"github.com/grpc/grpc-go"
 )
 
 
-type timesheetService struct {
+type TimeSheetService struct {
+}
+
+func New() *TimeSheetService {
+	return &TimeSheetService{}
+}
+
+func (tss *TimeSheetService) ClockIn(ctx context.Context, _ *api.ClockRequest) (api.SimpleResponse, error) {
+	timeEntry := &TimeEntry{}
+	timeEntry.UserID
 }
