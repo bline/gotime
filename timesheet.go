767db8b9410bc59ba8cdcdda110e1c9efffa0306
Scott Beck <sbeck@shambhalamountain.org>
2017-11-30 11:27:15 -0700

    mocked up interface

--- timesheet.go
+++ timesheet.go
@@ -2,23 +2,32 @@ package gotime
 
 import (
 	"context"
-
-	"github.com/bline/gotime"
 	"github.com/bline/gotime/api/proto"
-	"github.com/bline/gotime/db"
-	"github.com/bline/gotime/config"
-	"github.com/grpc/grpc-go"
+	"github.com/golang/protobuf/ptypes"
+	"time"
+	"log"
 )
 
 
 type TimeSheetService struct {
 }
 
-func New() *TimeSheetService {
-	return &TimeSheetService{}
+func (tss *TimeSheetService) ClockIn(ctx context.Context, _ *api.ClockRequest) (*api.SimpleResponse, error) {
+	return &api.SimpleResponse{IsSuccess: true, Message: "Clock-In Success"}, nil
 }
 
-func (tss *TimeSheetService) ClockIn(ctx context.Context, _ *api.ClockRequest) (api.SimpleResponse, error) {
-	timeEntry := &TimeEntry{}
-	timeEntry.UserID
+func (tss *TimeSheetService) ClockOut(context.Context, *api.ClockRequest) (*api.SimpleResponse, error) {
+	return &api.SimpleResponse{IsSuccess: true, Message: "Clock-Out Success"}, nil
+
+}
+func (tss *TimeSheetService) GetCurrentStatus(context.Context, *api.ClockRequest) (*api.TSStatusResponse, error) {
+	ts, err := ptypes.TimestampProto(time.Now())
+	if err != nil {
+		log.Fatal("Bad time")
+	}
+	return &api.TSStatusResponse{State: 1, Timestamp: ts}, nil
+}
+
+func (tss *TimeSheetService) GetEntries(*api.TimeSheetRequest, api.TimeSheet_GetEntriesServer) error {
+	return nil
 }
