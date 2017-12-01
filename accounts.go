47249bb8282e3ac0ba3eb958424b41cfc4733026
bline <scottbeck@gmail.com>
2017-11-30 20:35:29 -0700

    oauth user from id token

--- accounts.go
+++ accounts.go
@@ -1,8 +1,13 @@
 package gotime
 
 import (
+	"github.com/bline/gotime/db"
 	"github.com/bline/gotime/api/proto"
+	"github.com/futurenda/google-auth-id-token-verifier"
 	"context"
+	"strings"
+	"errors"
+	"time"
 )
 
 type AccountsService struct {}
@@ -21,4 +26,48 @@ func (*AccountsService) DeleteUser(ctx context.Context, r *api.DeleteUserRequest
 }
 func (*AccountsService) LockUser(ctx context.Context, r *api.LockUserRequest) (*api.SimpleResponse, error) {
 	return &api.SimpleResponse{IsSuccess: true, Message: "User Locked"}, nil
+}
+
+func NewUserFromIDToken(idtoken string) (*User, error) {
+	conn := db.GetDB()
+	var curUser User
+	set, err := verifyToken(idtoken)
+	if err != nil {
+		return nil, err
+	}
+	conn.Where("GoogleID = ?", set.Sub).First(&curUser)
+	if curUser.ID != 0 {
+		return &curUser, nil
+	}
+	newUser := &User{
+		Email: set.Email,
+		GoogleID: set.Sub,
+		GivenName: set.GivenName,
+		FamilyName: set.FamilyName,
+		LastLogin: time.Now(),
+		Picture: set.Picture,
+		DisplayName: set.Name,
+		IsDisabled: false,
+		IsAdmin: false,
+	}
+	conn.Create(&newUser)
+	return newUser, nil
+}
+
+func verifyToken(token string) (*googleAuthIDTokenVerifier.ClaimSet, error) {
+	v := googleAuthIDTokenVerifier.Verifier{}
+	aud := "33812767661-4a1p5lotkkveeodjehfpkucvmbpkmkhf.apps.googleusercontent.com"
+	err := v.VerifyIDToken(token, []string{
+		aud,
+	})
+	if err != nil {
+		return nil, err
+	}
+	claimSet, err := googleAuthIDTokenVerifier.Decode(TOKEN)
+	// claimSet.Iss,claimSet.Email ... (See claimset.go)
+	// XXX get hd from PrivateClaim?
+	if !strings.HasSuffix(claimSet.Email, "@shambhalamountain.org") {
+		return nil, errors.New("must use a shambhalamountain.org email address")
+	}
+	return claimSet, nil
 }
